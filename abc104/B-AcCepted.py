4889d063cf966505a58590b0b48ba4398b8b6255
Your Name <71997636+yattemiyou@users.noreply.github.com>
2022-12-24 07:54:26 +0900

    abc104

--- abc104/B-AcCepted.py
+++ abc104/B-AcCepted.py
@@ -1,11 +1,14 @@
-S = input()
+def get_answer(S):
+    if S[0] != 'A':
+        return False
 
-if S[0] != 'A' or S[2:-1].count('C') != 1:
-    print('WA')
-else:
-    index = S.find('C')
+    if S[2:-1].count('C') != 1:
+        return False
 
-    if S[1:index].islower() and S[index+1:].islower():
-        print('AC')
-    else:
-        print('WA')
+    if sum(map(str.isupper, S)) != 2:
+        return False
+
+    return True
+
+
+print('AC' if get_answer(input()) else 'WA')
