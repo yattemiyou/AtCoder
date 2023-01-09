c39a2964144e7e03c65e04742cbc5f75371aa96e
Your Name <71997636+yattemiyou@users.noreply.github.com>
2023-01-09 13:53:23 +0900

    sample

--- _sample/input.py
+++ _sample/input.py
@@ -1,3 +1,21 @@
+from collections import defaultdict
+
+#
+N = int(input())
+print(N)
+
+#
+N, M = map(int, input().split())
+print(N, M)
+
+#
 N = int(input())
 
-N, M = map(int, input().split())
+graph = defaultdict(lambda: list())
+
+for _ in range(N):
+    A, B = map(int, input().split())
+
+    graph[A].append(B)
+    graph[B].append(A)
+print(graph)
