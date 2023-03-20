93d394d00619ddb52724e323377612b303160713
Your Name <71997636+yattemiyou@users.noreply.github.com>
2023-03-20 15:34:40 +0900

    sample

--- _sample/search.py
+++ _sample/search.py
@@ -37,3 +37,32 @@ def rfind_character(d, c, start):
     # 最前列かどうか
     return d[c][index-1] if index > 0 else -1
 ###
+
+
+###
+def merge():
+    A = [3, 14, 15, 92]
+    B = [6, 53, 58]
+    N = len(A)
+    M = len(B)
+
+    C = (N + M) * [0]
+
+    i, j = 0, 0
+    for index in range(N + M):
+        if A[i] < B[j]:
+            C[index] = A[i]
+            i += 1
+
+            if i >= N:
+                C[index + 1:] = B[j:]
+                break
+        else:
+            C[index] = B[j]
+            j += 1
+
+            if j >= M:
+                C[index + 1:] = A[i:]
+                break
+    return C
+###
